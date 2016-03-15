0f5ec6605adc94db9c57aad09df43f94ce69daaa
Erik Bean <Erik.Bean@cdk.com>
2016-03-15 16:00:17 -0700

    add spec runner in node

---
+++ specs/example.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe("Mocha in Electron", function () {
+    it("should assert with chai.expect", function () {
+        expect(true).to.be.true;
+    });
+});
